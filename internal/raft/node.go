53f0f33e1abd282d1b382819e8747e0bf97de0c6
Mohamed Yattara <yattaramohamed004@gmail.com>
2026-07-04 23:29:58 -0400

    feat: define Node and LogEntry structs with NewNode constructor

---
+++ internal/raft/node.go
@@ -0,0 +1,69 @@
+package raft
+
+import (
+	"sync"
+	"time"
+	 "math/rand"
+)
+
+type Node struct {
+	// identity
+    id      string
+    address string
+    peers   []string
+
+    // raft state
+    role        string
+    currentTerm int
+    votedFor    string
+
+    // log
+    log         []LogEntry
+    commitIndex int
+    lastApplied int
+
+    // key-value store
+    kvStore map[string]string
+
+    // leader only
+    nextIndex  map[string]int
+    matchIndex map[string]int
+
+    // coordination
+    mu          sync.Mutex
+    heartbeatCh chan bool
+    voteCh      chan bool
+    stepDownCh  chan bool
+
+    // timing
+    electionTimeout  time.Duration
+    heartbeatTimeout time.Duration
+}
+type LogEntry struct{
+	Term int
+	Command string
+}
+
+func NewNode(id string, address string, peers []string) *Node {
+    return &Node{
+		id:               id,
+        address:          address,
+        peers:            peers,
+        role:             "follower",
+        currentTerm:      0,
+        votedFor:         "",
+        log:              []LogEntry{},
+        commitIndex:      0,
+        lastApplied:      0,
+        kvStore:          make(map[string]string),
+        nextIndex:        make(map[string]int),
+        matchIndex:       make(map[string]int),
+        heartbeatCh:      make(chan bool, 1),
+        voteCh:           make(chan bool, 1),
+        stepDownCh:       make(chan bool, 1),
+        electionTimeout:  time.Duration(150+rand.Intn(150)) * time.Millisecond,
+        heartbeatTimeout: 50 * time.Millisecond,
+    }
+
+    }
+}
